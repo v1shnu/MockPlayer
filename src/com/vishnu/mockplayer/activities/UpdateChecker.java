b7840b987deb0ccd36ef067289bc240ee3afff2c
Vishnu <vishnumohandas@gmail.com>
2013-10-04 12:28:41 +0530

    Initialized new branch.

---
+++ src/com/vishnu/mockplayer/activities/UpdateChecker.java
@@ -0,0 +1,17 @@
+package com.vishnu.mockplayer.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: root
+ * Date: 4/10/13
+ * Time: 12:28 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class UpdateChecker extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
